8aac1c69b727839b8bb279c4627f686238fe6463
Cameron Devine <camerondtrain@gmail.com>
2018-07-01 13:09:52 -0700

    removed the print statements

--- python/StateModelRnD.py
+++ python/StateModelRnD.py
@@ -37,66 +37,48 @@ def find(InVars, StVarElEqns, OtherElEqns, Constraints, OutputVars):
 	# Constrains: A string of constraint (continuity and compatability) equations seperated by commas.
 	# 
 	InVars = sympify_vec(InVars)
-	if p: print 'InVars:\n', InVars
 
 	OutputVars = sympify_vec(OutputVars)
-	if p: print 'OutputVars:\n', OutputVars
 
 	StVarElEqns = sympify_eqs(StVarElEqns)
-	if p: print 'StVarElEqns:\n', StVarElEqns
 
 	OtherElEqns = sympify_eqs(OtherElEqns)
-	if p: print 'OtherElEqns:\n', OtherElEqns
 
 	Constraints = sympify_eqs(Constraints)
-	if p: print 'Constraints:\n', Constraints
 
 	#print "Symbolic Expressions Created"
 
 	StVars = [integrate(eq.lhs, t) for eq in StVarElEqns]
-	if p: print 'StVars:\n', StVars
 
 	PriVars = [eq.lhs for eq in OtherElEqns]
-	if p: print 'PriVars:\n', PriVars
 
 	SecVars = [eq.lhs for eq in Constraints]
-	if p: print 'SecVars:\n', SecVars
 
 	#print "Variable Collected"
 
 	func_subs = dict([(sympify(str(var).replace('(t)', '')), var) if '(t)' in str(var) else (var, sympify(str(var) + '(t)')) for var in InVars + StVars + PriVars + SecVars])
-	if p: print 'func_subs:\n', func_subs
 
 	StVarElEqns = [eq.subs(func_subs) for eq in StVarElEqns]
-	if p: print 'StVarElEqns:\n', StVarElEqns
 
 	OtherElEqns = [eq.subs(func_subs) for eq in OtherElEqns]
-	if p: print 'OtherElEqns:\n', OtherElEqns
 
 	StVars = [eq.subs(func_subs) for eq in StVars]
-	if p: print 'StVars:\n', StVars
 
 	PriVars = [eq.subs(func_subs) for eq in PriVars]
-	if p: print 'PriVars:\n', PriVars
 
 	SecVars = [eq.subs(func_subs) for eq in SecVars]
-	if p: print 'SecVars:\n', SecVars
 
 	#print "Functions Created"
 
 	Constraints = [eq.subs(func_subs) for eq in Constraints]
-	if p: print 'Constraints:\n', Constraints
 
 	DConstraints_sub = dict(
 		[(const.lhs, const.rhs) for const in Constraints]
 		+[(const.lhs.diff(t), const.rhs.diff(t)) for const in Constraints if const.lhs.diff(t) != 0])
-	if p: print 'DConstraints_sub:\n', DConstraints_sub
 
 	St2 = [Eq(eq.lhs, eq.rhs.subs(DConstraints_sub)) for eq in StVarElEqns]
-	if p: print 'St2:\n', St2
 
 	Co2 = [eq.subs(DConstraints_sub) for eq in OtherElEqns]
-	if p: print 'Co2:\n', Co2
 
 	E3 = {}
 	for eq1 in Co2:
@@ -105,29 +87,24 @@ def find(InVars, StVarElEqns, OtherElEqns, Constraints, OutputVars):
 			if eq1 != eq2:
 				eq = eq.subs(eq2.lhs, eq2.rhs)
 		E3[eq.lhs] = solve(eq, eq.lhs)[0]
-	if p: print 'E3:\n', E3
 
 	StateEquation = St2
 	_StateEquation = None
 	while StateEquation != _StateEquation:
 		_StateEquation = StateEquation
 		StateEquation = [eq.subs(E3) for eq in StateEquation]
-	if p: print 'StateEquation:\n', StateEquation
 
 	StateEqsFinal = [simplify(solve(eq.doit(), st.diff(t))[0]) for (eq, st) in zip(StateEquation, StVars)]
-	if p: print 'StateEqsFinal:\n', StateEqsFinal
 
 	OutputSubs = dict([(st.diff(t), eq) for (eq, st) in zip(StateEqsFinal, StVars)])
 	OutputSubs.update(DConstraints_sub)
 	OutputSubs.update(E3)
-	if p: print 'OutputSubs:\n', OutputSubs
 
 	OutputEqsFinal = OutputVars
 	_OutputEqsFinal = None
 	while OutputEqsFinal != _OutputEqsFinal:
 		_OutputEqsFinal = OutputEqsFinal
 		OutputEqsFinal = [eq.subs(OutputSubs) for eq in OutputEqsFinal]
-	if p: print 'OutputEqsFinal:\n', OutputEqsFinal
 
 	#print "Equations Solved"
 
@@ -155,11 +132,3 @@ def find(InVars, StVarElEqns, OtherElEqns, Constraints, OutputVars):
 	OutputEqsFinalMat = Matrix([OutputEqsFinal]).T
 
 	return {'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F, 'Bp': Bp, 'Dp': Dp, 'TF': TF, 'StateVec': StVec, 'OutputVec': OutVec, 'StateEq': StateEqsFinalMat, 'OutEq': OutputEqsFinalMat, 'InputVec': InVec}
-
-if __name__ == '__main__':
-	find(
-		"vS",
-		"vMB' = 1/MB * fMB, vMW' = 1/MW * fMW, fKS' = KS * vKS, fKT' = KT * vKT",
-		"fBS = BS * vBS, fBT = BT * vBT",
-		"fMB = fKS + fBS, fMW = fKT + fBT - fKS - fBS, vKS = vMW - vMB, vKT = vS - vMW, vBS = vMW - vMB, vBT = vS - vMW",
-		"vMB, vMW, fKS, fKT, fBS, fBT")
