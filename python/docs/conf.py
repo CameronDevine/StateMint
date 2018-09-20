307256e9032916bafed441e71fac720f876b8054
Cameron Devine <camerondtrain@gmail.com>
2018-09-20 13:33:02 -0700

    rolled the version number to 0.4.2

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -26,7 +26,7 @@ author = u'Cameron Devine'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.4.1'
+release = u'0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
