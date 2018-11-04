e749cd62f57e82d9bb5767f46e64689bcd798106
Cameron Devine <camerondtrain@gmail.com>
2018-11-03 23:37:20 -0700

    starting to work on the python documentation

---
+++ python/StateModelRnD/numpy.py
@@ -0,0 +1,47 @@
+'''Convert symbolic matricies to numpy matricies.
+
+This module provides convienience functions for turning symbolic matracies into numpy matricies for simulation and analysis.
+'''
+
+import numpy as np
+from sympy import symbols
+
+def array(data, values = {}):
+	'''Convert a symbolic matrix to a Numpy array.
+
+	Converts a given symbolic matrix, most likely returned in the output of `StateModelRnD.Solve`, into a Numpy array.
+
+	Args:
+		data (sympy.Matrix): The symbolic matrix to convert to a Numpy array.
+		values (dict of str: float): The values to replace each symbolic variable with in a dictionary with the key as a string of the variable name, and the value as the number to replace it with
+
+	Returns:
+		numpy.ndarray: A Numpy array of the matrix using the values given
+	'''
+	subs = {}
+	for val in values:
+		if val is str:
+			subs[symbols(val)] = values[val]
+		else:
+			subs[val] = values[val]
+	return np.array(data.subs(subs)).astype(np.float64)
+
+def matrix(data, values = {}):
+	'''Convert a symbolic matrix to a Numpy matrix.
+
+	Converts a given symbolic matrix, most likely returned in the output of `StateModelRnD.Solve`, into a Numpy matrix.
+
+	Args:
+		data (sympy.Matrix): The symbolic matrix to convert to a Numpy matrix.
+		values (dict of str: float): The values to replace each symbolic variable with in a dictionary with the key as a string of the variable name, and the value as the number to replace it with
+
+	Returns:
+		numpy.matrix: A Numpy matrix using the values given
+	'''
+	subs = {}
+	for val in values:
+		if val is str:
+			subs[symbols(val)] = values[val]
+		else:
+			subs[val] = values[val]
+	return np.matrix(data.subs(subs)).astype(np.float64)
--- python/docs/index.rst
+++ python/docs/index.rst
@@ -1,21 +1,4 @@
-.. StateModelRnD documentation master file, created by
-   sphinx-quickstart on Tue Aug 21 14:21:28 2018.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to StateModelRnD's documentation!
-=========================================
-
-his library is used to take the elemental equations and constraint equations of a system and find a differential equation in a standard form. This script is a port from a Mathematica notebook with the same functionality but was ported to allow it to be run using an AWS Lambda function, allowing anyone to run the code without having Python/sympy or Mathematica installed.
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
-Documentation
+StateModelRnD
 =============
 
 .. toctree::
@@ -24,5 +7,5 @@ Documentation
 
 .. automodule:: StateModelRnD
 	:members:
-.. automodule:: StateModelRnD.to_numpy
+.. automodule:: StateModelRnD.numpy
 	:members:
