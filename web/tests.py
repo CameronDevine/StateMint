da8a0abacb82500503d10f7778c6ed39c63c5356
Cameron Devine <camerondtrain@gmail.com>
2018-11-30 14:31:45 -0800

    for some reason I finally looked up how to do this

--- web/tests.py
+++ web/tests.py
@@ -394,9 +394,8 @@ class TestWebInterface(unittest.TestCase):
 
 	def test_add_image(self):
 		self.driver.find_element('id', 'fileUploadHidden').send_keys(os.getcwd() + '/HTML/examples/example1.jpg')
-		self.driver.wait(2)
+		self.driver.wait()
 		self.assertTrue(len(self.driver.find_element('id', 'systemImage').get_attribute('src')) > 0)
-		self.driver.save_screenshot('test.png')
 		for entry in self.driver.get_log('browser'):
 			print entry
 		self.assertTrue(self.driver.find_element('id', 'systemImage').is_displayed())
