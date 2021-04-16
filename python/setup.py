1db7ae56ea78846ae522aea5bab4937c04e66729
Cameron Devine <camerondtrain@gmail.com>
2021-04-15 19:53:13 -0700

    rolling the version to v2.1.1

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
 	name="StateMint",
-	version="2.1.0",
+	version="2.1.1",
 	author="Cameron Devine",
 	author_email="camdev@uw.edu",
 	description="A library for finding State Space models of dynamical systems.",
